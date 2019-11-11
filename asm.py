b0f0bc3cca520c6a64168529583b42ce3e93a096
Dale Humby <dale@nomanini.com>
2019-11-11 22:17:43 +0200

    Basic symbol resolution for variables and defines All symbols must be defined before use

--- asm.py
+++ asm.py
@@ -46,48 +46,58 @@ def parse(tokens):
 def machine(opcode, operands):
     """Create the machine code from the opcode and operands."""
     if opcode in ['stop', 'rts', 'clc', 'sec', 'incx']:
-        return _op(opcode)
+        return op(opcode)
     elif opcode in ['ldi', 'ldd', 'std']:
-        return _op_reg_abs8(opcode, operands[0], operands[1])
+        return op_reg_abs8(opcode, operands[0], operands[1])
     elif opcode in ['ldx', 'stx']:
-        return _op_reg(opcode, operands[0])
+        return op_reg(opcode, operands[0])
     elif opcode == 'mov':
-        return _op_reg_reg(opcode, operands[0], operands[1])
+        return op_reg_reg(opcode, operands[0], operands[1])
     elif opcode in ['bra', 'bcs', 'bcc', 'beq', 'bne', 'bsr']:
-        return _op_abs11(opcode, operands[0])
+        return op_abs11(opcode, operands[0])
     elif opcode in ['add', 'adc', 'and', 'or', 'xor']:
-        return _op_reg_reg_reg(opcode, operands[0], operands[1], operands[2])
+        return op_reg_reg_reg(opcode, operands[0], operands[1], operands[2])
     elif opcode in ['not', 'lsl', 'lsr', 'inc', 'dec']:
-        return _op_reg_reg(opcode, operands[0], operands[1])
+        return op_reg_reg(opcode, operands[0], operands[1])
     elif opcode == 'cmp':
         # A compare is the same as an XOR, but in this case we discard the result
-        return _op_reg_reg_reg('xor', 'a', operands[0], operands[1], compare=True)
+        return op_reg_reg_reg('xor', 'a', operands[0], operands[1], compare=True)
     else:
         return 65535
 
-def _op(opcode):
+def op(opcode):
     return instruction[opcode] << 11
 
-def _op_reg(opcode, R):
+def op_reg(opcode, R):
     return instruction[opcode] << 11 | register[R] << 8
 
-def _op_reg_reg(opcode, Rd, Rs):
+def op_reg_reg(opcode, Rd, Rs):
     return instruction[opcode] << 11 | register[Rd] << 8 | register[Rs] << 4
 
-def _op_reg_reg_reg(opcode, Rd, Rs1, Rs2, compare=False):
+def op_reg_reg_reg(opcode, Rd, Rs1, Rs2, compare=False):
     m = instruction[opcode] << 11 | register[Rd] << 8 | register[Rs1] << 4 | register[Rs2]
     if compare:
         m = m | (1 << 7)  # if a compare, use bit 7 to tell ALU to discard result
     return m
 
-def _op_reg_abs8(opcode, R, abs):
-    abs = int(abs, 0)
+
+def resolve_symbol(s):
+    try:
+        return int(str(s), 0)
+    except ValueError:
+        try:
+            return resolve_symbol(symbol[s])
+        except KeyError:
+            raise Exception(f"Undefined symbol '{s}'")
+
+def op_reg_abs8(opcode, R, abs):
+    abs = resolve_symbol(abs)
     if abs >= 255:
         raise Exception('Absolute value must be less than 255')
     return instruction[opcode] << 11 | register[R] << 8 | abs
 
-def _op_abs11(opcode, abs):
-    abs = int(abs, 0)
+def op_abs11(opcode, abs):
+    abs = resolve_symbol(abs)
     if abs >= 2048:
         raise Exception('Absolute value must be less than 2048')
     return instruction[opcode] << 11 | abs
@@ -95,29 +105,48 @@ def _op_abs11(opcode, abs):
 
 def machine2string(m):
     """Turn the machine code in to a printable string."""
-    s = bin(m)[2:]  # strip 0b off the string
-    s = s.rjust(16, '0')  # make the bin representation 16 bits
+    s = format(m, '016b')
     return s[0:5] + ' ' + s[5:8] + ' ' + s[8:12] + ' ' + s[12:16]  # chunk so easy to read
 
 
 if __name__ == "__main__":
+    symbol = {}
+    address = 0
+
     with open(args.filename, 'r') as f:
         for line in f.readlines():
             line = line.lstrip().rstrip().lower()
-            if len(line) == 0:
+            if not line:
                 # Empty line
                 pass
             elif line[0] == ';':
                 # Comments can be skipped
                 pass
             elif line[0] == '.':
-                # Handle . commands
-                # todo
-                pass
+                define = re.search(r"\.define\s+(\w+)\s+(\w+)", line)
+                data = re.search(r"\.data\s+(\w+)\s+(\w+)(?:$|\s+\{(.*)\})", line)
+                if define:
+                    groups = define.groups()
+                    symbol[groups[0]] = groups[1]
+                elif data:
+                    groups = data.groups()
+                    symbl = groups[0]
+                    symbol[symbl] = address
+                    byte_count = resolve_symbol(groups[1])
+                    print(f'{format(address, "04x")} : {symbl}[{byte_count}]')
+                    address += byte_count
+                    values = groups[2]
+                    if values:
+                        v = values.split(',')
+                        v = [int(y) for y in v]
+                else:
+                    raise Exception(f'Cannot parse line {line}')
             else:
                 # Assume to be assembly code
                 tokens = tokenise(line)
                 opcode, operands = parse(tokens)
                 m = machine(opcode, operands)
-                print(f'{machine2string(m)}\t: {opcode} {operands}')
+                print(f'{format(address, "04x")} : {machine2string(m)}\t| {opcode} {operands}')
+                address += 2
 
+    print(symbol)
---
+++ symbol_data.asm
@@ -0,0 +1,25 @@
+; File with no defines, values, etc., just raw addresses
+
+.define 	LENGTH 	2
+.define 	VAL 	0b00001010
+
+.data 		temp	LENGTH
+.data 		fib 	0x03
+.data 		array 	5 {1,2,3, 4, 5}
+
+LDI		A, 0
+LDI		B, 5
+ADD		C, A, B 		; C = A + B
+STD		C, temp		   	; Store C to address 0x00A0
+CMP		A, B
+BEQ		0x0000
+LDD		A, fib
+LDI		B, VAL
+ADD		C, A, B
+INC		D, C
+LDD		B, array
+ADC		D, B, C
+MOV		C, D
+BRA		0x0000			; loop back to start
+BRA		0b1111
+STOP					; Halt
