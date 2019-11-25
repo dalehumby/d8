6c77694185f116fde66edadfd411d2b460a096e2
Dale Humby <dale@nomanini.com>
2019-11-25 22:07:55 +0200

    Refactor gui in to functions in prep for adding in emulator code

--- gui.py
+++ gui.py
@@ -1,6 +1,67 @@
 import curses
 
-def draw_menu(stdscr):
+def handle_command(screen):
+    """Handle command mode."""
+    # Create command bar
+    height, width = screen.getmaxyx()
+    screen.attron(curses.color_pair(1))
+    screen.addstr(height-1, 0, ':' + " " * (width - 2))
+    screen.move(height - 1, 1)  # Put cursor in status bar
+    curses.curs_set(1)  # Switch cursor on
+    curses.echo()
+    screen.refresh()
+    input = screen.getstr(height-1, 1, 20)
+    curses.noecho()
+    curses.curs_set(0)
+    screen.addstr(height-1, 0, " "*(width-1))
+    screen.attroff(curses.color_pair(1))
+
+def handle_step(pad, source):
+    """Handle a single step of the CPU."""
+    # todo: step CPU
+
+    # Update row highlighting to next row
+    pad.attron(curses.color_pair(2))
+    pad.addstr(handle_step.step_row, 1, f'{handle_step.step_row:2d} {handle_step.step_row:04X}  {source[handle_step.step_row]}')
+    handle_step.step_row += 1
+    pad.attron(curses.color_pair(1))
+    pad.addstr(handle_step.step_row, 1, f'{handle_step.step_row:2d} {handle_step.step_row:04X}  {source[handle_step.step_row]}')
+
+handle_step.step_row = 0
+
+def draw_registers(win):
+    win.attron(curses.color_pair(2))
+    win.addstr(0, 0, 'Registers')
+    win.addstr(1, 0, 'S:0  C:0  Z:1')
+    win.addstr(2, 0, 'A    255 0xFF b10101100')
+    win.addstr(3, 0, 'B    b10101100 0xFF 255')
+    win.addstr(4, 0, 'C    b10101100 0xFF 255')
+    win.addstr(5, 0, 'D    b10101100 0xFF 255')
+    win.addstr(6, 0, 'X    b10101100 0xFF 255')
+    win.addstr(7, 0, 'SPCH b10101100 0xFF 255')
+    win.addstr(8, 0, 'SPCL b10101100 0xFF 255')
+    win.addstr(9, 0, 'PC   0xFFFF')
+    win.addstr(10, 0, 'INST 11100 111 0111 0111')
+    win.noutrefresh()
+
+def draw_variables(win):
+    win.attron(curses.color_pair(2))
+    win.addstr(0, 0, 'Variables')
+    win.addstr(1, 0, 'temp[1]: [0]')
+    win.addstr(2, 0, 'fib[10]: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]')
+    win.addstr(3, 0, 'return[2]: [0, 0]')
+    win.noutrefresh()
+
+def draw_memory(win):
+    win.attron(curses.color_pair(2))
+    win.addstr(0, 0, 'Memory')
+    win.addstr(1, 0, '0000 00 11 22 33 44 55 66 77')
+    win.addstr(2, 0, '0008 00 11 22 33 44 55 66 77')
+    win.addstr(3, 0, '0010 00 11 22 33 44 55 66 77')
+    win.addstr(4, 0, '0018 00 11 22 33 44 55 66 77')
+    win.noutrefresh()
+
+def run_emulator(stdscr):
     k = 0
 
     # Clear and refresh the screen for a blank canvas
@@ -20,102 +81,60 @@ def draw_menu(stdscr):
     for y, text in enumerate(source):
         source_pad.addstr(y, 1, f'{y:2d} {y:04X}  {text}')
     top_row = 0
-    step_row = 0
 
     # Registers window
-    registers_win_height = 11
-    registers_win_width = 30
-    registers_win = curses.newwin(registers_win_height, registers_win_width, 1, width-registers_win_width)
+    reg_win_height = 11
+    reg_win_width = 30
+    reg_win = curses.newwin(reg_win_height, reg_win_width, 1, width-reg_win_width)
 
     # Variables window
     var_win_height = 5
-    var_win_width = registers_win_width
-    var_win = curses.newwin(var_win_height, var_win_width, registers_win_height+2, width-var_win_width)
+    var_win_width = reg_win_width
+    var_win = curses.newwin(var_win_height, var_win_width, reg_win_height+2, width-var_win_width)
 
     # Memory window
     # todo: Memory on a line on its own, with a pad below so can scroll around memory
     mem_win_height = 5
-    mem_win_width = registers_win_width
-    mem_win = curses.newwin(mem_win_height, mem_win_width, registers_win_height+var_win_height+2, width-mem_win_width)
+    mem_win_width = reg_win_width
+    mem_win = curses.newwin(mem_win_height, mem_win_width, reg_win_height+var_win_height+2, width-mem_win_width)
 
+     # Render title bar
+    stdscr.attron(curses.color_pair(1))
+    stdscr.addstr(0, 0, filename)
+    stdscr.addstr(0, len(filename), " " * (width - len(filename) - 1))
+    stdscr.attroff(curses.color_pair(1))
+
+    # Render the command bar
+    curses.curs_set(0)
+    stdscr.attron(curses.color_pair(1))
+    stdscr.addstr(height-1, 0, " "*(width-1))
+    stdscr.attroff(curses.color_pair(1))
 
     # Loop where k is the last character pressed
     while (k != ord('q')):
 
-        # Initialization
-        #stdscr.clear()
-        height, width = stdscr.getmaxyx()
-
-         # Render title bar
-        stdscr.attron(curses.color_pair(1))
-        stdscr.addstr(0, 0, filename)
-        stdscr.addstr(0, len(filename), " " * (width - len(filename) - 1))
-        stdscr.attroff(curses.color_pair(1))
-
-        # Render status bar
-        statusbarstr = ':'
-        stdscr.attron(curses.color_pair(1))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(1))
-
-        # Put cursor in status bar
-        stdscr.move(height - 1, len(statusbarstr) + 1)
-
-
         # Code window
-        if k == curses.KEY_DOWN:
+        if k == ord(':'):
+            handle_command(stdscr)
+        elif k == curses.KEY_DOWN:
             top_row += 1
             top_row = min(len(source)-height+2, top_row)
         elif k == curses.KEY_UP:
             top_row -= 1
             top_row = max(0, top_row)
         elif k == ord('s'):
-            source_pad.attron(curses.color_pair(2))
-            source_pad.addstr(step_row, 1, f'{step_row:2d} {step_row:04X}  {source[step_row]}')
-            step_row += 1
-            source_pad.attron(curses.color_pair(1))
-            source_pad.addstr(step_row, 1, f'{step_row:2d} {step_row:04X}  {source[step_row]}')
+            handle_step(source_pad, source)
 
         # Breakpoints
+        # todo: update breakpoints from the command mode
         source_pad.attron(curses.color_pair(3))
         source_pad.addch(21, 0, '●', curses.A_BOLD)
+        source_pad.noutrefresh(top_row, 0, 1, 0, height-1, width-reg_win_width)
 
-        source_pad.noutrefresh(top_row, 0, 1, 0, height-1, width-registers_win_width)
-
-
-        # Registers window
-        registers_win.attron(curses.color_pair(2))
-        registers_win.addstr(0, 0, 'Registers')
-        registers_win.addstr(1, 0, 'S:0  C:0  Z:1')
-        registers_win.addstr(2, 0, 'A    255 0xFF b10101100')
-        registers_win.addstr(3, 0, 'B    b10101100 0xFF 255')
-        registers_win.addstr(4, 0, 'C    b10101100 0xFF 255')
-        registers_win.addstr(5, 0, 'D    b10101100 0xFF 255')
-        registers_win.addstr(6, 0, 'X    b10101100 0xFF 255')
-        registers_win.addstr(7, 0, 'SPCH b10101100 0xFF 255')
-        registers_win.addstr(8, 0, 'SPCL b10101100 0xFF 255')
-        registers_win.addstr(9, 0, 'PC   0xFFFF')
-        registers_win.addstr(10, 0, 'INST 11100 111 0111 0111')
-        registers_win.noutrefresh()
-
-
-        # Variables windown
-        var_win.attron(curses.color_pair(2))
-        var_win.addstr(0, 0, 'Variables')
-        var_win.addstr(1, 0, 'temp[1]: [0]')
-        var_win.addstr(2, 0, 'fib[10]: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]')
-        var_win.addstr(3, 0, 'return[2]: [0, 0]')
-        var_win.noutrefresh()
-
-        # Memory window
-        mem_win.attron(curses.color_pair(2))
-        mem_win.addstr(0, 0, 'Memory')
-        mem_win.addstr(1, 0, '0000 00 11 22 33 44 55 66 77')
-        mem_win.addstr(2, 0, '0008 00 11 22 33 44 55 66 77')
-        mem_win.addstr(3, 0, '0010 00 11 22 33 44 55 66 77')
-        mem_win.addstr(4, 0, '0018 00 11 22 33 44 55 66 77')
-        mem_win.noutrefresh()
+        # Update screen
+        draw_registers(reg_win)
+        draw_variables(var_win)
+        draw_memory(mem_win)
 
         # Refresh the screen
         curses.doupdate()
@@ -132,4 +151,4 @@ if __name__ == "__main__":
         source = f.readlines()
     source = [ line.rstrip() for line in source ]
 
-    curses.wrapper(draw_menu)
+    curses.wrapper(run_emulator)
